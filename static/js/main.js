5ac19caab489722099453e7f0c638028550a639f
csandras <andras.kovacs98@gmail.com>
2020-02-19 10:49:32 +0100

    randomized enemies

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,14 @@
 function myMove() {
+    let targetList = ["targetLeft1", "targetLeft2", "targetRight1", "targetRight2"];
     let elem = document.getElementById("target");
     let pos = 0;
     let id = setInterval(frame, 10);
     function frame() {
-        if (pos > 88) {
+        if (pos > 85) {
             clearInterval(id);
             elem.style.top = '0px';
+            document.getElementById("target").className =
+                targetList[Math.floor(Math.random() * (4 - 0 + 1)) + 0];
             myMove()
         } else {
             pos += 0.2;
