2be428f5f4c6bfab28801a6b052ac296c5642a32
mark frojimovics <frojimovicsm@gmail.com>
2020-02-20 14:59:21 +0100

    machete thrown

--- static/js/main.js
+++ static/js/main.js
@@ -24,66 +24,72 @@ let KEY_RIGHT = 39;
 document.addEventListener('keydown', keypress, false);
 document.addEventListener('keydown', switchPics, false);
 
+var macheteThrown = false;
+
 function keypress(event) {
+    if (macheteThrown === false) {
 
-    if (event.keyCode === KEY_LEFT) {
-        var macheteLeft = document.getElementById("machete-left");
-        var jason = document.getElementById("jason");
-        var jasonWithoutMachete = document.getElementById("jason-without-machete");
-        var macheteRight = document.getElementById("machete-right");
-        var knifeSound = document.getElementById("knife-sound");
-        var pos = 0;
-        let pos2 = 0;
-        var id_left = setInterval(frameLeft,5);
-        var jason_m = setInterval(jasonMachete,5);
-        var jason_nm = setInterval(jasonNoMachete,5);
+        if (event.keyCode === KEY_LEFT) {
+            macheteThrown = true;
+            var macheteLeft = document.getElementById("machete-left");
+            var jason = document.getElementById("jason");
+            var jasonWithoutMachete = document.getElementById("jason-without-machete");
+            var macheteRight = document.getElementById("machete-right");
+            var knifeSound = document.getElementById("knife-sound");
+            var pos = 0;
+            let pos2 = 0;
+            var id_left = setInterval(frameLeft, 5);
+            var jason_m = setInterval(jasonMachete, 5);
+            var jason_nm = setInterval(jasonNoMachete, 5);
 
 
-        function frameLeft() {
-            knifeSound.play();
-            knifeSound.volume = 0.2;
-            if (pos === 600) {
-                clearInterval(id_left);
-                macheteLeft.style.top = '40px';
-                macheteLeft.style.left = '5px';
-            } else {
-                pos += 3;
-                pos2 -= 3.8;
-                macheteLeft.style.top = pos + 'px';
-                macheteLeft.style.left = pos2 + "px";
-                console.log(pos)
+            function frameLeft() {
+                knifeSound.play();
+                knifeSound.volume = 0.2;
+                if (pos === 600) {
+                    clearInterval(id_left);
+                    macheteLeft.style.top = '40px';
+                    macheteLeft.style.left = '5px';
+                    macheteThrown = false;
+                } else {
+                    pos += 3;
+                    pos2 -= 3.8;
+                    macheteLeft.style.top = pos + 'px';
+                    macheteLeft.style.left = pos2 + "px";
+                    console.log(pos)
+                }
+
             }
+        } else if (event.keyCode === KEY_RIGHT) {
+            var macheteLeft = document.getElementById("machete-left");
+            var jason = document.getElementById("jason");
+            var jasonWithoutMachete = document.getElementById("jason-without-machete");
+            var macheteRight = document.getElementById("machete-right");
+            var knifeSound = document.getElementById("knife-sound");
+            var pos = 0;
+            let pos2 = 0;
+            var id_right = setInterval(frameRight, 5);
 
+            function frameRight() {
+                knifeSound.play();
+                knifeSound.volume = 0.2;
+                if (pos === 600) {
+                    clearInterval(id_right);
+                    macheteRight.style.top = '40px';
+                    macheteRight.style.left = "0px";
+                    macheteThrown = false;
+                } else {
+                    pos += 3;
+                    pos2 += 3.8;
+                    macheteRight.style.top = pos + 'px';
+                    macheteRight.style.left = pos2 + "px";
+                    console.log(pos);
+                }
+            }
         }
     }
-    else if (event.keyCode === KEY_RIGHT) {
-        var macheteLeft = document.getElementById("machete-left");
-        var jason = document.getElementById("jason");
-        var jasonWithoutMachete = document.getElementById("jason-without-machete");
-        var macheteRight = document.getElementById("machete-right");
-        var knifeSound = document.getElementById("knife-sound");
-        var pos = 0;
-        let pos2 = 0;
-        var id_right = setInterval(frameRight, 5);
 
-        function frameRight() {
-            knifeSound.play();
-            knifeSound.volume = 0.2;
-            if (pos === 600) {
-                clearInterval(id_right);
-                macheteRight.style.top = '40px';
-                macheteRight.style.left = "0px";
-            } else {
-                pos += 3;
-                pos2 += 3.8;
-                macheteRight.style.top = pos + 'px';
-                macheteRight.style.left = pos2 + "px";
-                console.log(pos);
-            }
-        }
-    }
 }
-
 function switchPics(e) {
     if (e.keyCode === KEY_LEFT) {
         var jason = document.getElementById("jason");
